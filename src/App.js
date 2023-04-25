ed083769424a578cbbfc7ba427844e543643134c
Mayukh <mayukhchatterjee2023@gmail.com>
2023-04-25 11:42:50 +0530

    Adding Meals Summary with hardcoded meal list

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import Header from "./Component/Layout/Header";
+import Meals from "./Component/Meals/Meals";
 
 function App() {
   return (
     <div>
       <h2>Let's get started!</h2>
       <Header/>
+      <main>
+        <Meals/>
+      </main>
     </div>
   );
 }
